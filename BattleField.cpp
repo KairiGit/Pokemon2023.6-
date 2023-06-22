84575d58931b43c540302894aa23b3bb318b3d81
KairiGit <130728369+KairiGit@users.noreply.github.com>
2023-06-22 11:10:44 +0900

    BattleStart

--- BattleField.cpp
+++ BattleField.cpp
@@ -1,21 +1,32 @@
 #include ".\baseInclude.h"
 
 
-BattleField::BattleField(Trainer* a,Trainer* b){
+    BattleField::BattleField(Trainer* a,Trainer* b){
         for(int i=0;i<3;i++){
             this->A->getMyPokemon(i)->setPokemon(a->getMyPokemon(i));
             this->B->getMyPokemon(i)->setPokemon(b->getMyPokemon(i));
         }
     }
+    
     void BattleField :: showPokemon(bool which){
         if(which){
             for(int i=0;i<3;i++){
-                std::cout << i+1 << A->getMyPokemon(i) << std::endl;
+                std::cout << i+1 << A->getMyPokemon(i)->getPokeName() << std::endl;
             }
         }
         else{
             for(int i=0;i<3;i++){
-                std::cout << i+1 << B->getMyPokemon(i) << std::endl;
+                std::cout << i+1 << B->getMyPokemon(i)->getPokeName() << std::endl;
             }
         }
+    }
+    void BattleField :: Battle(){
+        std::cout<<B->getTrainerName()<<" sent out ";
+        std::cout<<B->getMyPokemon(1)->getPokeName();
+        std::cout<<'!'<<std::endl;
+
+        std::cout<<"Go! ";
+        std::cout<<A->getMyPokemon(1)->getPokeName()
+        std::cout<<'!'<<std::endl;
+
     }
